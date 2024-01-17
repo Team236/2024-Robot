a275cb65d310badd664adfa4198eba12d80ff6a5
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-17 14:08:57 -0500

    commented out sensitivity (only use if we revert back to 2023 arcade drive)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,8 +40,8 @@ public final class Constants {
     public static final double LEFT_DEADZONE = 0.05; //0.15???
     public static final double RIGHT_DEADZONE = 0.05;
 
-    public static final double SENSITIVITY_X = 0.9;
-    public static final double SENSITIVITY_Y = 0.9; //TODO  try 0.75
+    //public static final double SENSITIVITY_X = 0.9;
+    //public static final double SENSITIVITY_Y = 0.9; //TODO  try 0.75
     //public static final boolean IS_DEADZONE = true;
 
     //external drive encoders
