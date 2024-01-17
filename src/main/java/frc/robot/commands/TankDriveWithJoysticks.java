68ed434d151ac69b5f25c785525c3f3443a85d18
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-17 13:16:51 -0500

    New command to tank drive with  2 joysticks

---
+++ src/main/java/frc/robot/commands/TankDriveWithJoysticks.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drive;
+
+public class TankDriveWithJoysticks extends Command {
+  /** Creates a new TankDriveWithJoysticks. */
+    private Drive drive;
+
+  public TankDriveWithJoysticks(Drive drive) {
+     this.drive = drive;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drive.openRampRate();
+    //drive.resetLeftEncoder();
+    //drive.resetRightEncoder();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drive.TankWithSticks();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
