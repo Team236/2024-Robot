839a6f10fec0328c901fc4da1eca104a42454131
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-17 14:09:38 -0500

    Updated to have different deadzones for Xbox and Joystick inputs

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -31,7 +31,7 @@ public class ArcadeDrive extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-   drive.ArcadeWithDeadzone();
+   drive.ArcadeWithXBox();
     }
 
   // Called once the command ends or is interrupted.
