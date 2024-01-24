cad63ff65fcb7236588232103492250a54362021
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-24 10:48:41 -0500

    Updated command name and method names.  Simplified to a single method.

---
+++ src/main/java/frc/robot/commands/CartridgeShooterCommands/ToStowedPosition.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+package frc.robot.commands.CartridgeShooterCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CartridgeShooter;
+
+public class ToStowedPosition extends Command {
+  private CartridgeShooter cartridgeShooter;
+
+  
+  /** Creates a new MoveCartridge. */
+  public ToStowedPosition(CartridgeShooter cartridgeShooter) {
+    this.cartridgeShooter = cartridgeShooter;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.cartridgeShooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  cartridgeShooter.cartridgeStowedPosition();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;  //*** Try false here if having trouble executing this command
+  }
+}
