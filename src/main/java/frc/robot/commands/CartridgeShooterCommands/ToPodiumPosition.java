2af8cd73a9147b4f43347aeb85f40bd78ec941bd
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-24 10:48:34 -0500

    Updated command name and method names.  Simplified to a single method.

---
+++ src/main/java/frc/robot/commands/CartridgeShooterCommands/ToPodiumPosition.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.CartridgeShooterCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CartridgeShooter;
+
+public class ToPodiumPosition extends Command { 
+  //Moves the cartridge to PodiumShot position using pnuematics
+
+   private CartridgeShooter cartridgeShooter;
+
+  public ToPodiumPosition(CartridgeShooter cartridgeShooter) {
+    this.cartridgeShooter = cartridgeShooter;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.cartridgeShooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    cartridgeShooter.podiumShotPosition();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;  //*** Try false here if having trouble executing this command
+  }
+}
