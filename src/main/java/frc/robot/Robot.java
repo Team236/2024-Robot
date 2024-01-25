a3cbda20d04b782d2b0bbd6da9de0347bf8821e2
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-25 07:34:52 -0500

    Moved Counter from Intake to Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -19,6 +21,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private Compressor compressor;
+  public Counter counter;
+  private boolean isCounterUnplugged = false;
 
   private RobotContainer m_robotContainer;
 
@@ -33,6 +37,15 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
     compressor = new Compressor(PneumaticsModuleType.CTREPCM);
     compressor.enableDigital();
+
+    try {
+      counter = new Counter();
+      counter.setUpSource(Constants.Intake.DIO_INTAKE_COUNTER);
+      counter.reset();
+    } catch (Exception e) {
+      isCounterUnplugged = true;
+    }
+
   }
 
   /**
@@ -49,6 +62,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putBoolean("Is counter unplugged? ", isCounterUnplugged);
+    SmartDashboard.putNumber("The counter count is:", counter.get());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
