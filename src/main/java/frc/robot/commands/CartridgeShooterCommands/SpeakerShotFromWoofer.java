9eeed0446595342831211a60b4675500585a2e29
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-01-25 08:39:40 -0500

    Added code to this command template

--- src/main/java/frc/robot/commands/CartridgeShooterCommands/SpeakerShotFromWoofer.java
+++ src/main/java/frc/robot/commands/CartridgeShooterCommands/SpeakerShotFromWoofer.java
@@ -4,13 +4,21 @@
 
 package frc.robot.commands.CartridgeShooterCommands;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.CartridgeShooter;
+
 
 public class SpeakerShotFromWoofer extends Command {
-  
 
-  public SpeakerShotFromWoofer() {
-  
+private CartridgeShooter cartridgeShooter; 
+private Robot robot;
+
+  public SpeakerShotFromWoofer(CartridgeShooter cartridgeShooter, Robot robot) {
+    this.cartridgeShooter = cartridgeShooter;
+    this.robot = robot;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.cartridgeShooter);
   }
 
   // Called when the command is initially scheduled.
@@ -19,13 +27,15 @@ public class SpeakerShotFromWoofer extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    cartridgeShooter.setBothSpeeds(Constants.CartridgeShooter.WOOFER_SHOT_MOTOR_SPEED);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    //***TODO  Add here - reset the counter to zero (do after all shots) - counter is in Robot.Java
-  
+      cartridgeShooter.setBothSpeeds(0);
+     //robot.counter.reset();  //try this after implementing TrapAmpShooter branch changes;
   }
 
   // Returns true when the command should end.
