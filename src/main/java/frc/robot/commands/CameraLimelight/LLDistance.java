315881da8f26c423f06d5ae776d3459adf1a8a4f
Steven Burnham <burnhamsf@region18.org>
2024-02-10 08:54:26 -0500

    LLDistance with LLHelpers implimentation

--- src/main/java/frc/robot/commands/CameraLimelight/LLDistance.java
+++ src/main/java/frc/robot/commands/CameraLimelight/LLDistance.java
@@ -4,9 +4,11 @@
 
 package frc.robot.commands.CameraLimelight;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Drive;
 
 public class LLDistance extends Command {
@@ -21,17 +23,20 @@ public class LLDistance extends Command {
 
   private double kY = 0.02; //0.00725;
   
+  
   private double h1 = 34;// approx ht now = was 32.5; //inches, from ground to center of camera lens
   //private double h2 = 18; // inches, same unit as d, to center of target
   private double a1 = Math.toRadians(6); //6 degrees, camera tilt
   private double dist; // desired distance from camera to target in inches; pass into command
   private Drive drive;
-  private double pipeline;
+  private int pipeline;
   private double targetHeight;//18" for Atag, from floor to center of target
   private double tv, disY, a2, dx, errorY;
+  private double[] limeTarget;
+  private double tag_id=0;
   
-  /** Creates a new LLAngle. */
-  public LLDistance(Drive drive, double pipeline, double standoff, double targetHeight) {
+  /** CONSTRUCTOR - Creates a new LLAngle. */
+  public LLDistance(Drive drive, int pipeline, double standoff, double targetHeight) {
     this.drive = drive;
     this.pipeline = pipeline;
     this.dist = standoff;
@@ -42,33 +47,68 @@ public class LLDistance extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    limeTarget = LimelightHelpers.getTargetPose_RobotSpace("limelight");
     SmartDashboard.putNumber("LLDistance init", pipeline);
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline);
+    SmartDashboard.putNumberArray("tag_target_array",LimelightHelpers.getTargetPose_RobotSpace("limelight"));
+    LimelightHelpers.setLEDMode_ForceOff("limelight");
+    LimelightHelpers.setPipelineIndex("limelight",pipeline);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-   NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
-   tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+     // this is already initialized to off might  be redundant
+   LimelightHelpers.setLEDMode_ForceOff("limelight");
+    // could be faster to return limelight x,y,z,xr,yr,zr 'Results' all at once
+    // this gets pose but not tag_id and all other data at once
+   limeTarget = LimelightHelpers.getTargetPose_RobotSpace("limelight"); 
+   
+   // there is no "has target=tv " for the april tags but current tag_id should be null if not found
+   //tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+   tag_id = LimelightHelpers.getFiducialID("limelight");
  
     // TO make sure dx is positive, use abs value for disY and (h1-h2)
-   disY = Math.abs (NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0));
+  //  disY = Math.abs (NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0));
 
-    if(tv==1){
-        a2 = disY*Math.PI/180; // in radians, if disY in degrees
-        dx = Math.abs(targetHeight - h1) / Math.tan(a1+a2);  
-        errorY = dist - dx;  
+   // if has_target should change to switch case with tag_id
+   // alternative is if then statement tag_id > 0 do something 
+   switch ((int)tag_id) {
+    case 1,2: 
+    // do samething for the 1,2 Red Blue Amp
+    driveDistance(Constants.Amp.AMP_TAG_DISTANCE);
+    break;
+    case 3, 4:
+      // do something for the 3,4 Red speaker 
+      driveDistance(Constants.Speaker.SPEAKER_TAG_DISTANCE);
+      break;
+    case 7, 8:
+      // do something for the 7,8 Blue speaker
+      driveDistance(Constants.Speaker.SPEAKER_TAG_DISTANCE);
+      break;
+    case 11,12,13:     // do something for the 11,12,13 Blue stage
+        // falls through to to next break if 'break' is missing
+    case 14,15,16:
+      // do something for the 14,15,16 Red stage
+      driveDistance(Constants.Stage.STAGE_TAG_DISTANCE);
+      break;
+    default:   // default used undeclared or nothing if no default action
+      break;      
+   }
+  }  
+    
+   private void driveDistance(double distance) {
+   if(tag_id>0){
+        dx = limeTarget[2];  // TODO verify position 2 Y-distance is correct axis for 'TargetPose_RobotSpace'   
+        errorY = distance - dx;  
     //NOTE:  CAN TRY TO USE THE Z VALUE OF THE POSE FOR errorY (use [2] or [0] for other directions)
       double distanceAdjust = kY * errorY;
-       drive.setLeftSpeed(-distanceAdjust); //change to negative since LL is in back of robot this year
+       drive.setLeftSpeed(-distanceAdjust);   //change to negative since LL is in back of robot this year
        drive.setRightSpeed(-distanceAdjust);  //change to negative since LL is in back of robot this year
       SmartDashboard.putNumber("dx, Y dist from target:", dx); //test this - use later for cartridge angle equation
       SmartDashboard.putNumber("ErrorY:", errorY);
       SmartDashboard.putNumber("Ty, degrees:", disY);
    } else{
-      SmartDashboard.putNumber("No Target", tv);
+      SmartDashboard.putNumber("No Target", tag_id);
    }
   }
 
