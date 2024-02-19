69bc9bf802fa5d90398772eddf8bc60ab3f1337d
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-02-19 16:11:52 -0500

    Add smartDash stmt in PIDCartridge Tilt (in comments) for bench testing.

--- src/main/java/frc/robot/commands/CartridgeAndTilt/PIDCartridgeTilt.java
+++ src/main/java/frc/robot/commands/CartridgeAndTilt/PIDCartridgeTilt.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.CartridgeAndTilt;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Cartridge;
@@ -56,6 +57,7 @@ public class PIDCartridgeTilt extends Command {
       isAtLimit = true; 
     }
     else isAtLimit = false;
+   // SmartDashboard.putBoolean("Tilt isFinished condition is: ", isAtLimit);
 
     return isAtLimit;
     }
