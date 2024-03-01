54408f032151f7c2af5bca12be008127cd033c65
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-03-01 11:59:08 -0500

    updated WooferLeft turn

--- src/main/java/frc/robot/commands/Autos/WooferLeft.java
+++ src/main/java/frc/robot/commands/Autos/WooferLeft.java
@@ -36,7 +36,7 @@ public class WooferLeft extends SequentialCommandGroup {
         ),
     new PIDDrive(drive, Constants.DriveConstants.WOOFER_PULL_AWAY).withTimeout(1),
     //new PIDTurn(drive, -Constants.DriveConstants.TURN_SIDE_OF_WOOFER).withTimeout(1.8),
-    new PIDTurnCCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER + 3).withTimeout(1.5),
+    new PIDTurnCCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER + 2).withTimeout(1.5),
     Commands.parallel(
         new PIDDrive(drive, Constants.DriveConstants.PULL_AWAY_TO_NOTE).withTimeout(2),
         new IntakeWithCounter(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2)     
@@ -44,7 +44,7 @@ public class WooferLeft extends SequentialCommandGroup {
   //TODO: CAN CHANGE TO SHOT FROM FAR (with one Turn first) WITH NO DRIVING REVERSE?
     new PIDDrive(drive, -Constants.DriveConstants.PULL_AWAY_TO_NOTE).withTimeout(1.5),
    // new PIDTurn(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER).withTimeout(2),
-    new PIDTurnCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER).withTimeout(1.5),
+    new PIDTurnCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER +2).withTimeout(1.5),
     Commands.parallel( 
       new PIDDrive(drive, -Constants.DriveConstants.WOOFER_PULL_AWAY-2).withTimeout(3),
       new PIDCartridgeShot(intake, cartridge, tilt, Constants.Intake.INTAKE_SPEED, Constants.CartridgeShooter.WOOFER_PID_RPM, Constants.Tilt.TILT_ENC_REVS_WOOFER).withTimeout(3)
