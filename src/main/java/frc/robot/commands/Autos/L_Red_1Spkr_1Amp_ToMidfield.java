9a65266b576704cef7a9a57d248d7c059f416316
Ellen DiCarlo <edicarlo@sbcglobal.net>
2024-03-06 08:34:45 -0500

    Updated 1Spkr/1Amp Autos to use AmpShotNoCartMotors (which tilts to Stow)

--- src/main/java/frc/robot/commands/Autos/L_Red_1Spkr_1Amp_ToMidfield.java
+++ src/main/java/frc/robot/commands/Autos/L_Red_1Spkr_1Amp_ToMidfield.java
@@ -36,24 +36,25 @@ public class L_Red_1Spkr_1Amp_ToMidfield extends ParallelCommandGroup {
       new PIDCartridgeMotors(cartridge, Constants.CartridgeShooter.WOOFER_PID_RPM).withTimeout(16),  //run cart motors in parallel with every command in Auto
       new PIDUptoHeight(elevator, Constants.Elevator.MATCH_HEIGHT).withTimeout(2),//bring elevator to match height (Start elev at bot limit at match start)
       Commands.sequence(
-        new PIDSpkrShotNoCart(intake, tilt, Constants.Intake.INTAKE_SPEED, Constants.Tilt.TILT_ENC_REVS_WOOFER), //times out in 2 sec. Enough for tilt/then intake? Can reduce?
+        new PIDSpkrShotNoCart(intake, tilt, Constants.Intake.INTAKE_SPEED, Constants.Tilt.TILT_ENC_REVS_WOOFER).withTimeout(2.3), 
         new PIDDrive(drive, Constants.DriveConstants.WOOFER_PULL_AWAY).withTimeout(1),
         new PIDTurnCCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER + 2).withTimeout(1.5),
         Commands.parallel(
          new PIDDrive(drive,  Constants.DriveConstants.PULL_AWAY_TO_NOTE).withTimeout(2),
-         new IntakeWithCounter(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
-         new PIDCartridgeTilt(tilt, Constants.Tilt.TILT_ENC_REVS_STOW).withTimeout(2)         
+         new IntakeWithCounter(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2)
+         //new PIDCartridgeTilt(tilt, Constants.Tilt.TILT_ENC_REVS_STOW).withTimeout(2)         
          ),
         new PIDDrive(drive, -37.9).withTimeout(1.5),
         new PIDTurnCCW (drive, 90).withTimeout(1.5),
         new PIDDrive(drive, -28.6).withTimeout(1.5),
         Commands.parallel(
-        new ManualIntake(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
-        new AmpMotor(ampTrap, Constants.Amp.AMP_TRAP_MOTOR_SPEED).withTimeout(2)
+          new AmpShotNoCartMotors(intake, ampTrap, tilt).withTimeout(2.1)
+          //new ManualIntake(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
+          //new AmpMotor(ampTrap, Constants.Amp.AMP_TRAP_MOTOR_SPEED).withTimeout(2)
         )
       )
     );
-    drive.setGearHigh();
+    //drive.setGearHigh();
     Intake.resetCounter();
   }
 }
--- src/main/java/frc/robot/commands/Autos/R_Blue_1Spkr_1Amp_ToMidfield.java
+++ src/main/java/frc/robot/commands/Autos/R_Blue_1Spkr_1Amp_ToMidfield.java
@@ -16,6 +16,7 @@ import frc.robot.commands.Drive.PIDTurnCW;
 import frc.robot.commands.Elevator.PIDUptoHeight;
 import frc.robot.commands.Intake.IntakeWithCounter;
 import frc.robot.commands.Intake.ManualIntake;
+import frc.robot.commands.Shots.AmpShotNoCartMotors;
 import frc.robot.commands.Shots.PIDSpkrShotNoCart;
 import frc.robot.subsystems.AmpTrap;
 import frc.robot.subsystems.Cartridge;
@@ -37,24 +38,25 @@ public class R_Blue_1Spkr_1Amp_ToMidfield extends ParallelCommandGroup {
       new PIDCartridgeMotors(cartridge, Constants.CartridgeShooter.WOOFER_PID_RPM).withTimeout(16),  //run cart motors in parallel with every command in Auto
       new PIDUptoHeight(elevator, Constants.Elevator.MATCH_HEIGHT).withTimeout(2),//bring elevator to match height (Start elev at bot limit at match start)
       Commands.sequence(
-        new PIDSpkrShotNoCart(intake, tilt, Constants.Intake.INTAKE_SPEED, Constants.Tilt.TILT_ENC_REVS_WOOFER),//times out in 2 sec. Enough? Can reduce?
+        new PIDSpkrShotNoCart(intake, tilt, Constants.Intake.INTAKE_SPEED, Constants.Tilt.TILT_ENC_REVS_WOOFER).withTimeout(2.3),
         new PIDDrive(drive, Constants.DriveConstants.WOOFER_PULL_AWAY).withTimeout(1),
         new PIDTurnCW(drive, Constants.DriveConstants.TURN_SIDE_OF_WOOFER).withTimeout(1.5),
         Commands.parallel(
          new PIDDrive(drive,  Constants.DriveConstants.PULL_AWAY_TO_NOTE).withTimeout(2),
-         new IntakeWithCounter(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
-         new PIDCartridgeTilt(tilt, Constants.Tilt.TILT_ENC_REVS_STOW).withTimeout(2)    
+         new IntakeWithCounter(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2)
+         //new PIDCartridgeTilt(tilt, Constants.Tilt.TILT_ENC_REVS_STOW).withTimeout(2)    
         ),   
         new PIDDrive(drive, -37.9).withTimeout(1.5),
         new PIDTurnCW (drive, 90).withTimeout(1.5),
         new PIDDrive(drive, -28.6).withTimeout(1.5),
         Commands.parallel(
-         new ManualIntake(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
-         new AmpMotor(ampTrap, Constants.Amp.AMP_TRAP_MOTOR_SPEED).withTimeout(2)
+         new AmpShotNoCartMotors(intake, ampTrap, tilt).withTimeout(2.1)
+         //new ManualIntake(intake, Constants.Intake.INTAKE_SPEED).withTimeout(2),
+         //new AmpMotor(ampTrap, Constants.Amp.AMP_TRAP_MOTOR_SPEED).withTimeout(2)
         )
        )
     );
-    drive.setGearHigh();
+    //drive.setGearHigh();
     Intake.resetCounter();
   }
 }
