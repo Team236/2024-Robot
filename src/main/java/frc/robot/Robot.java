e19ff1c799e4289fc4e05e648eb0903a527d5e14
Elena Gerardo <mikedefiore742@gmail.com>
2024-03-23 10:04:40 -0400

    camera code in robot.java added back

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,14 +52,13 @@ public class Robot extends TimedRobot {
     compressor.enableDigital();
     
   //USB camera and servo:
-   //cameraServo = new Servo(Constants.PWM_FRONT_CAM);
+   cameraServo = new Servo(Constants.PWM_FRONT_CAM);
    // default the camera to point at front floor as starting position
-//
-	//	try {
-  //    usbCamera0 = CameraServer.startAutomaticCapture(0);
-   // } catch (Exception e)  {
-   // SmartDashboard.putString("camera capture failed", "failed");
-  //  }
+	try {
+    usbCamera0 = CameraServer.startAutomaticCapture(0);
+    } catch (Exception e)  {
+   SmartDashboard.putString("camera capture failed", "failed");
+    }
 
     //Need to do this once only in order to have Limelight communication while tethered
     for (int port = 5800; port <= 5805; port++){
